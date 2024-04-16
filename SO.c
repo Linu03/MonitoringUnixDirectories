738273b7114b577d42feeac740f6388aa67bdce0
Linu03 <163161692+Linu03@users.noreply.github.com>
2024-04-16 20:27:58 +0300

    Add files via upload
    Adaugarea functionalitatii de a avea -o iesire oriunde ca argument in linia de comanda dupa ./exe , si adaugare in snapshot a continutului unui subdirector.

--- SO.c
+++ SO.c
@@ -10,37 +10,15 @@
 
 #define MAX_DIR 10
 
-void createSnapshot(const char *cale, const char *outputDir) {
+void createSnapshotRecursive(const char *cale, const char *outputDir, int snapshotFile) {
     DIR *dir;
     struct dirent *entry;
     struct stat fileInfo;
     char fullPath[1024];
-    char snapshotPath[2048];
     char buffer[2048];
 
     if (!(dir = opendir(cale))) {
- printf("Eroare la deschiderea directorului \%s.\n", cale);
-        return;
-    }
-
-    //obt nume dir
-    char *lastSlash = strrchr(cale, '/');
-    const char *dirName;  // Modificat aici
-
-    if (lastSlash) {
-        dirName = lastSlash + 1;
-    } else {
-        dirName = cale;
-    }
-
-    // creare nume
-    snprintf(snapshotPath, sizeof(snapshotPath), "%s/%s_snapshot.txt", outputDir, dirName);
-
-    int snapshotFile = open(snapshotPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-    if (snapshotFile == -1) {
-        printf("Eroare deschidere fisier de snapshot \"%s\".\n", snapshotPath);
-        closedir(dir);
+        printf("Eroare la deschiderea directorului %s.\n", cale);
         return;
     }
 
@@ -52,7 +30,7 @@ void createSnapshot(const char *cale, const char *outputDir) {
         snprintf(fullPath, sizeof(fullPath), "%s/%s", cale, entry->d_name);
 
         if (lstat(fullPath, &fileInfo) == -1) {
-            perror("Eroare info fisier.\n");
+            perror("Eroare la accesarea informatiilor fisierului.\n");
             continue;
         }
 
@@ -67,32 +45,58 @@ void createSnapshot(const char *cale, const char *outputDir) {
 
         bytes_written = snprintf(buffer, sizeof(buffer), "Inode: %ld\n\n", fileInfo.st_ino);
         write(snapshotFile, buffer, bytes_written);
+
+        if (S_ISDIR(fileInfo.st_mode)) {
+            createSnapshotRecursive(fullPath, outputDir, snapshotFile);
+        }
     }
 
-    close(snapshotFile);
     closedir(dir);
 }
 
+
+void createSnapshot(const char *cale, const char *outputDir) {
+    char snapshotPath[2048];
+    snprintf(snapshotPath, sizeof(snapshotPath), "%s/%s_snapshot.txt", outputDir, cale);
+
+    int snapshotFile = open(snapshotPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (snapshotFile == -1) {
+        printf("Eroare la deschiderea fisierului de snapshot %s.\n", snapshotPath);
+        return;
+    }
+
+    createSnapshotRecursive(cale, outputDir, snapshotFile);
+
+    close(snapshotFile);
+}
+
+
 int main(int argc, char *argv[]) {
-    if (argc <=3 || argc > MAX_DIR + 3) {
+    if (argc <= 3 || argc > MAX_DIR + 4) {
         printf("Numar invalid de argumente.\n");
         exit(-1);
     }
 
     const char *outputDir = NULL;
-    int dirsEndIndex = argc - 2;
 
-    if (strcmp(argv[dirsEndIndex], "-o") != 0) {
-        printf("Nu exista -o .\n");
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-o") == 0) {
+            outputDir = argv[i + 1];
+        }
+    }
+
+    if (!outputDir) {
+        printf("Nu exista -o sau directorul de iesire.\n");
         exit(-1);
     }
 
-    outputDir = argv[dirsEndIndex + 1];
-
-    for (int i = 1; i < dirsEndIndex; i++) {
+    for (int i = 1; i < argc - 2; i++) {
+        if (strcmp(argv[i], "-o") == 0) {
+	  i += 2; 
+        }
         createSnapshot(argv[i], outputDir);
     }
-
 
     printf("All good all done. =)\n");
     return 0;
